b5725df35200eb71717d63407243eea2a9ed315d
Wiktor Jóźwik <wiktor.jozwik@flip.shop>
2024-03-25 09:04:20 +0100

    Add smaller logo and information about page WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <main className='flex flex-col items-center justify-center w-full h-scren'>
       <Navigation />
+        <h1 className="pt-5 text-3xl">PAGE IN PROGRESS</h1>
+
       <About />
       <Expertise />
       {/* Projects */}
--- src/components/navigation/Logo.tsx
+++ src/components/navigation/Logo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image'
 
-import logo from '../../../public/logo-512x512.png';
+import logo from '../../../public/logo-192x192.png';
 
 
 const Logo = () => {
