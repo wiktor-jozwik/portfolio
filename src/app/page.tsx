77a5c771a2b152f8bdd597168f2a0201a4a2abd1
Wiktor Jóźwik <wiktor.jozwik@flip.shop>
2024-03-28 19:00:17 +0100

    Improve footer's socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,6 @@ export default function Home() {
   return (
     <main className='flex flex-col items-center justify-center w-full h-scren'>
       <Navigation />
-      <h1 id='test' className='pt-5 text-3xl'>
-        PAGE IN PROGRESS
-      </h1>
-
       <About />
       <Expertise />
       <Projects />
