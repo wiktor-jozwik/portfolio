03e2edb0bbb8f8de76a82f2413eec97d9a1aa9bb
Wiktor Jóźwik <wiktor.jozwik@flip.shop>
2024-03-28 20:25:13 +0100

    Add image

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import Summary from './Summary';
+import Image from 'next/image';
+
+import MeImage from '../../../public/me.jpg';
+
 const About = () => {
   return (
     <div>
       <div id='about' className='relative -top-8'></div>
       <div className='pt-16 flex flex-col sm:flex-row'>
-        <div className='flex items-center justify-center sm:justify-end sm:w-8/12 lg:w-7/12'>
+        <div className='flex items-center justify-center md:justify-end md:w-8/12 lg:w-7/12'>
           <Summary />
         </div>
-        <div className='hidden sm:flex items-center justify-center pt-8 sm:w-4/12 lg:w-5/12'>
-          Image
+        <div className='hidden md:flex sm:w-5/12 justify-center'>
+          <Image
+            className='w-72 justify-end aspect-auto rounded-full'
+            src={MeImage}
+            alt={'Image of Wiktor Jóźwik'}
+          />
         </div>
       </div>
     </div>
