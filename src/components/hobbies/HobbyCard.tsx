afe9b1ab665a9272a8fe58682c295340c6983544
Wiktor Jóźwik <wiktor.jozwik@flip.shop>
2024-03-28 20:05:39 +0100

    Change padding of hobbies cards

--- src/components/hobbies/HobbyCard.tsx
+++ src/components/hobbies/HobbyCard.tsx
@@ -4,7 +4,7 @@ import { HobbyData } from '@/interfaces';
 interface HobbyCardProps extends HobbyData {}
 const HobbyCard = ({ name, description }: HobbyCardProps) => {
   return (
-    <div className='m-4 p-2 pb-2.5 bg-gradient-to-b from-primary/65 to-primary/5 rounded-lg'>
+    <div className='m-4 px-3 py-12 bg-gradient-to-b from-primary/65 to-primary/5 rounded-lg'>
       <h3 className='text-center pb-1.5'>{name}</h3>
       <p className='text-center text-xs text-primary-text'>{description}</p>
     </div>
