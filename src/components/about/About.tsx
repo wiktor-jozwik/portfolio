fa70c6819fb9a1d35596bc78580005b931984d34
Wiktor Jóźwik <wiktor.jozwik@flip.shop>
2024-04-04 19:44:19 +0200

    Show ME image only on large screen

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -12,7 +12,7 @@ const About = () => {
         <div className='flex items-center justify-center md:justify-end md:w-8/12 lg:w-7/12'>
           <Summary />
         </div>
-        <div className='hidden md:flex sm:w-5/12 justify-center'>
+        <div className='hidden lg:flex sm:w-5/12 justify-center'>
           <Image
             className='w-72 justify-end aspect-auto rounded-full'
             src={MeImage}
